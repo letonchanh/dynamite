a3479c817769fa9bade1d0e99019379129394792
chanhle <letonchanh@gmail.com>
2019-08-21 09:25:32 -0400

    added DIG settings

--- src/dynamo.py
+++ src/dynamo.py
@@ -2,18 +2,16 @@ import os
 import sys
 import time
 
-cwd = os.getcwd()
-os.chdir("../deps/dig/src")
-dig_path = os.getcwd()
-sys.path.append(dig_path)
-os.chdir(cwd)
+dynamo_path = os.path.realpath(os.path.dirname(__file__))
+dig_path = os.path.realpath(os.path.join(dynamo_path, '../deps/dig/src'))
+sys.path.insert(0, dig_path)
 
 def run(inp, seed, maxdeg, do_rmtmp):
-    import alg
+    import alg as dig_alg
     if inp.endswith(".java") or inp.endswith(".class"):
-        dig = alg.DigSymStates(inp)
+        dig = dig_alg.DigSymStates(inp)
     else:
-        dig = alg.DigTraces(inp)
+        dig = dig_alg.DigTraces(inp)
     invs, traces, tmpdir = dig.start(seed, maxdeg)
 
     if do_rmtmp:
@@ -24,31 +22,34 @@ def run(inp, seed, maxdeg, do_rmtmp):
         print("tmpdir: {}".format(tmpdir))
 
 if __name__ == "__main__":
-    import alg as dig_alg
     import settings as dig_settings
     import argparse
 
     aparser = argparse.ArgumentParser("Dynamo")
     ag = aparser.add_argument
     ag("inp", help="inp")
-    ag("--log_level", "-log_level",
+    ag("--dig_log_level", "-dig_log_level",
        type=int,
        choices=range(5),
        default=2,
-       help="set logger info")
-    ag("--seed", "-seed",
+       help="DIG: set logger info")
+    ag("--dig_seed", "-dig_seed",
        type=float,
-       help="use this seed")
-    ag("--nomp", "-nomp",
+       help="DIG: use this seed in DIG")
+    ag("--dig_nomp", "-dig_nomp",
        action="store_true",
-       help="don't use multiprocessing")
+       help="DIG: don't use multiprocessing")
 
     args = aparser.parse_args()
 
-    import settings
-    settings.DO_MP = not args.nomp
+    dig_settings.DO_MP = not args.dig_nomp
+
+    import helpers.vcommon as dig_common_helpers
+    if 0 <= args.dig_log_level <= 4 and args.dig_log_level != dig_settings.logger_level:
+        dig_settings.logger_level = args.dig_log_level
+    dig_settings.logger_level = dig_common_helpers.getLogLevel(dig_settings.logger_level)
 
     inp = os.path.realpath(os.path.expanduser(args.inp))
-    seed = round(time.time(), 2) if args.seed is None else float(args.seed)
+    seed = round(time.time(), 2) if args.dig_seed is None else float(args.dig_seed)
 
     run(inp, seed, maxdeg=1, do_rmtmp=True)
