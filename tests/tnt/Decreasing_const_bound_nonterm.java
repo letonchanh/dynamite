928cba0e9812a1dcedc274c3ebfaac72b46d17f9
chanhle <letonchanh@gmail.com>
2019-08-27 11:04:57 -0400

    instrument loops with bnd

--- tests/tnt/Decreasing_const_bound_nonterm.java
+++ tests/tnt/Decreasing_const_bound_nonterm.java
@@ -8,15 +8,20 @@ public class Decreasing_const_bound_nonterm {
     }
 
     public static void main (String[] args) {
+        int bnd = Integer.parseInt(args[0]);
         int x = Nondet.getInt();
-        mainQ(x);
+        mainQ(bnd, x);
     }
 
-    public static void mainQ(int x) {
+    public static void mainQ(int bnd, int x) {
         while (x >= 0) {
+            if (bnd <= 0) break;
+            else
+                bnd = bnd - 1;
             vtrace1(x);
             x = x + 1;
         }
-        vtrace2(x);
+        if (bnd > 0)
+            vtrace2(x);
     }
 }
--- tests/tnt/run.sh
+++ tests/tnt/run.sh
@@ -11,16 +11,16 @@ javac $TEST.java
 echo "$TRACE: $VARS" > ${TEST}_${TRACE}.tcs
 for i in `seq $NUM 1`;
 do
-    java $TEST > $TEST.out &
-    while true; do
-        if grep -q 'vtrace2' $TEST.out; then #LOOP EXIT
-           break
-        elif (( $(grep -c 'vtrace1' $TEST.out) > $BND )); then
-            kill $(jps | grep $TEST | awk '{print $1}')
-            break
-        else
-            sleep 0.1
-        fi
-    done
+    java $TEST $BND > $TEST.out
+    # while true; do
+    #     if grep -q 'vtrace2' $TEST.out; then #LOOP EXIT
+    #        break
+    #     elif (( $(grep -c 'vtrace1' $TEST.out) > $BND )); then
+    #         kill $(jps | grep $TEST | awk '{print $1}')
+    #         break
+    #     else
+    #         sleep 0.1
+    #     fi
+    # done
     grep $TRACE $TEST.out >> ${TEST}_${TRACE}.tcs
 done
