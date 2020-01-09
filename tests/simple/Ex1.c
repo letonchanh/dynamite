d2197748c4cabe4f75879b2ce5e82fa880019b73
chanhle <letonchanh@gmail.com>
2020-01-09 14:44:13 +0000

    using lldb to get decls info from binaries

--- tests/simple/Ex1.c
+++ tests/simple/Ex1.c
@@ -12,17 +12,11 @@ void vtrace3(int x, int y) {
 void vtrace4(int x0, int y0, int x1, int y1) {
 }
 
-void mainQ(int x, int y) {
-  // assert (bnd > 0);
+void mainQ_loop(int x, int y) {
+  
   vtrace1(x, y);
-  // int bnd = 500;
-  // int counter = 0;
 
   while (x >= 0) {
-    // if (counter >= bnd)
-    //   break;
-    // else
-    //   counter++;
     vtrace2(x, y);
 
     int x0 = x;
@@ -35,7 +29,6 @@ void mainQ(int x, int y) {
     int y1 = y;
     vtrace4(x0, y0, x1, y1);
   }
-  // if (counter < bnd)
   vtrace3(x, y);
 }
 
