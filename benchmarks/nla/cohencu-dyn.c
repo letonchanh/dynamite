cf865d37c35fbcdec92f1ed602ccc1176a3d64dd
Timos Antonopoulos <timos@ip-172-31-63-194.us-west-2.compute.internal>
2020-03-17 15:51:00 +0000

    nla transformations

--- benchmarks/nla/cohencu-dyn.c
+++ benchmarks/nla/cohencu-dyn.c
@@ -2,47 +2,50 @@
 Printing consecutive cubes, by Cohen
 http://www.cs.upc.edu/~erodri/webpage/polynomial_invariants/cohencu.htm
 */
+#include<stdio.h>
+#include <stdlib.h>
 
-extern void __VERIFIER_error() __attribute__((__noreturn__));
-extern int __VERIFIER_nondet_int(void);
-extern void __VERIFIER_assume(int expression);
-void __VERIFIER_assert(int cond) {
-    if (!(cond)) {
-    ERROR:
-        __VERIFIER_error();
-    }
-    return;
+void vtrace1(int x, int y, int z, int k, int c) {
 }
 
-int main() {
-    int a, n, x, y, z;
-    a = __VERIFIER_nondet_int();
-    n = 0;
-    x = 0;
-    y = 1;
-    z = 6;
+void vtrace2(int x, int y, int z, int k, int c) {
+}
 
-    while (1) {
-        __VERIFIER_assert(z == 6 * n + 6);
-        __VERIFIER_assert(y == 3 * n * n + 3 * n + 1);
-        __VERIFIER_assert(x == n * n * n);
-	__VERIFIER_assert(y*z - 18*x - 12*y + 2*z - 6 == 0);
-	__VERIFIER_assert((z*z) - 12*y - 6*z + 12 == 0);
-        if (!(n <= a))
+void vtrace3(int x, int y, int z, int k, int c) {
+}
+
+void vloop(int a, int n, int x, int y, int z) {
+    vtrace1(a, n, x, y, z);
+    int bnd = 500;
+    int counter = 0;
+
+    while (n<=a) {
+        if (counter >= bnd)
             break;
-
+        else
+            counter++;
+        vtrace2(a, n, x, y, z);
         n = n + 1;
         x = x + y;
         y = y + z;
         z = z + 6;
     }
+    if (counter < bnd) {
+        vtrace3(a, n, x, y, z);
+    }
+}
 
-    __VERIFIER_assert(z == 6*n + 6);
-    __VERIFIER_assert(6*a*x - x*z + 12*x == 0);
-    __VERIFIER_assert(a*z - 6*a - 2*y + 2*z - 10 == 0);
-    __VERIFIER_assert(2*y*y - 3*x*z - 18*x - 10*y + 3*z - 10 == 0);
-    __VERIFIER_assert(z*z - 12*y - 6*z + 12 == 0);
-    __VERIFIER_assert(y*z - 18*x - 12*y + 2*z - 6 == 0);
-    
-    return 0;
+void mainQ(int a) {
+    int n, x, y, z;
+    n = 0;
+    x = 0;
+    y = 1;
+    z = 6;
+
+    vloop(a, n, x, y, z);
+
+}
+
+void main(int argc, char **argv){
+  mainQ(atoi(argv[1]), atoi(argv[2]));
 }
