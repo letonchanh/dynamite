a20e27f4a37c98334cebd07c13233f7abd3ecc98
chanhle <letonchanh@gmail.com>
2020-03-25 14:16:46 +0000

    add new sqrt1 example for dyn

---
+++ benchmarks/svcomp-nla-digbench/sqrt1-dyn-t.c
@@ -0,0 +1,51 @@
+// https://github.com/sosy-lab/sv-benchmarks/blob/master/c/nla-digbench/sqrt1.c
+/* Compute the floor of the square root of a natural number */
+#include<stdio.h>
+#include <stdlib.h>
+
+void vtrace1(int a, int t, int s, int n) {
+}
+
+void vtrace2(int a, int t, int s, int n) {
+}
+
+void vtrace3(int a, int t, int s, int n) {
+}
+
+void vloop(int a, int t, int s, int n) {
+    vtrace1(a, t, s, n);
+    int bnd = 500;
+    int counter = 0;
+
+    while ((a + 1) * (a + 1) <= n) {
+    // while ()
+        if (counter >= bnd)
+            break;
+        else
+            counter++;
+        vtrace2(a, t, s, n);
+
+        a = a + 1;
+        t = t + 2;
+        s = s + t;
+    }
+
+    if (counter < bnd) {
+        vtrace3(a, t, s, n);
+    }
+}
+
+void mainQ(int n) {
+    int a, s, t;
+
+    a = 0;
+    s = 1;
+    t = 1;
+
+    vloop(a, t, s, n);
+
+}
+
+void main(int argc, char **argv) {
+    mainQ(atoi(argv[1]));
+}
