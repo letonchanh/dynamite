d09fa81dded50316a8d6503555aba811ecb867fb
ThanhVu Nguyen <tnguyen@ip-172-31-63-194.us-west-2.compute.internal>
2020-03-25 02:04:19 +0000

    add assert1, works with /tools/ultimate/run.pl taipan reeachability ~/dynamo/benchmarks/svcomp-nla-digbench/sqrt1-ult.c

--- benchmarks/svcomp-nla-digbench/sqrt1-ult.c
+++ benchmarks/svcomp-nla-digbench/sqrt1-ult.c
@@ -1,5 +1,6 @@
 // https://github.com/sosy-lab/sv-benchmarks/blob/master/c/nla-digbench/sqrt1.c
 /* Compute the floor of the square root of a natural number */
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
 
 extern void abort(void); 
 void reach_error(){}
@@ -16,6 +17,14 @@ void __VERIFIER_assert(int cond) {
     return;
 }
 
+void __VERIFIER_assert1(int cond) {
+    if (!(cond)) {
+	ERROR: __VERIFIER_error();
+    }
+    return;
+}
+
+
 
 int main() {
     int n, a, s, t;
@@ -26,9 +35,9 @@ int main() {
     t = 1;
 
     while (s <= n) {
-      //__VERIFIER_assert(t == 2*a + 1);
-      //__VERIFIER_assert(s == (a + 1) * (a + 1));
-      //__VERIFIER_assert(t*t - 4*s + 2*t + 1 == 0);
+	__VERIFIER_assert1(t == 2*a + 1);
+	__VERIFIER_assert1(s == (a + 1) * (a + 1));
+	__VERIFIER_assert1(t*t - 4*s + 2*t + 1 == 0);
         // the above 2 should be equiv to 
       //if (!(s <= n))break;
         a = a + 1;
