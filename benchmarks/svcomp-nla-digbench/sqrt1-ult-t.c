54778280c33edc370eb5167561ab92573009f0c4
Ton-Chanh Le <letonchanh@gmail.com>
2020-03-26 20:22:05 +0000

    remove assert/assume from sqrt1-ult-t.c

--- benchmarks/svcomp-nla-digbench/sqrt1-ult-t.c
+++ benchmarks/svcomp-nla-digbench/sqrt1-ult-t.c
@@ -31,7 +31,7 @@ int main() {
     while (t*t - 4*s + 2*t + 1 + c <= k) {
       //__VERIFIER_assert(t == 2*a + 1);
       //__VERIFIER_assert(s == (a + 1) * (a + 1));
-      __VERIFIER_assert(t*t - 4*s + 2*t + 1 == 0);
+      //__VERIFIER_assert(t*t - 4*s + 2*t + 1 == 0);
         // the above 2 should be equiv to 
       //if (!(s <= n))break;
         a = a + 1;
