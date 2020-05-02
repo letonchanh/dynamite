b99dd4ae42a24c6583bf136222f25d4ec4a7971b
chanhle <letonchanh@gmail.com>
2020-05-02 02:18:39 +0000

    add an example

---
+++ tests/simple/Ex1i-main.c
@@ -0,0 +1,16 @@
+void main(int argc, char **argv) {
+  int x, y;
+  while (x >= 0) {
+    x = x + y;
+    y = y + 1;
+  }
+}
+
+/*
+  // The original code
+  while (x >= 0) {
+    x = x + y;
+    y = y + 1;
+  }
+ */
+
