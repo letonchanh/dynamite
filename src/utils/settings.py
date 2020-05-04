f21c0ca111c0cf4ad19a9d6f1e4bae0e3b243bba
chanhle <letonchanh@gmail.com>
2020-05-04 06:42:08 +0000

    validate witness with Ult

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -46,20 +46,28 @@ class CPAchecker:
     CPA_HOME = Path(os.path.expandvars("$CPA_HOME"))
     CPA_EXE = CPA_HOME / 'scripts' / 'cpa.sh'
     
-    CPA_COMMON_OPTS = "-spec {spec}".format(spec=REACHABILITY.SPEC)
+    CPA_COMMON_OPTS = "-spec {spec} -setprop cpa.predicate.encodeBitvectorAs=INTEGER".format(spec=REACHABILITY.SPEC)
     CPA_REACH_OPTS = "-predicateAnalysis -setprop counterexample.export.compressWitness=false"
     CPA_VALIDATE_OPTS = partial("-witnessValidation -witness {witness}".format)
     
-    CPA_CMD = partial("{cpa_exe} {cpa_opts} {cpa_task_opts} {input}".format,
-                      cpa_exe=CPA_EXE, cpa_opts=CPA_COMMON_OPTS)
+    CPA_CMD = partial("{cpa_exe} {cpa_opts} {cpa_task_opts} {input}".format)
+    CPA_RUN = partial(CPA_CMD, cpa_exe=CPA_EXE, cpa_opts=CPA_COMMON_OPTS)
 
 class Ultimate:
     ULT_HOME = Path(os.path.expandvars("$ULT_HOME"))
     ULT_EXE = lambda variant: Ultimate.ULT_HOME / 'releaseScripts' / 'default' / ('{variant}-linux'.format(variant=variant)) / 'Ultimate.py'
 
-    ULT_COMMON_OPTS = partial("--spec {spec} --architecture {arch}bit --witness-dir {witness_dir} --witness-name {witness_name}".format, spec=REACHABILITY.SPEC, arch=REACHABILITY.ARCH)
-    
-    ULT_CMD = lambda variant, witness_dir, witness_name, input: "{ult_exe} {ult_opts} --file {input}".format(
-                                        ult_exe=Ultimate.ULT_EXE(variant=variant), 
-                                        ult_opts=Ultimate.ULT_COMMON_OPTS(witness_dir=witness_dir, witness_name=witness_name),
-                                        input=input)
+    ULT_COMMON_OPTS = "--spec {spec} --architecture {arch}bit".format(spec=REACHABILITY.SPEC, arch=REACHABILITY.ARCH)
+    ULT_REACH_OPTS = partial("--witness-dir {witness_dir} --witness-name {witness_name}".format)
+    ULT_VALIDATE_OPTS = partial("--validate {witness_dir}/{witness_name}".format)
+
+    ULT_REACH_TASK = 'REACH'
+    ULT_VALIDATE_TASK = 'VALIDATE'
+
+    ULT_CMD = partial("{ult_exe} {ult_opts} {ult_task_opts} --file {input}".format)
+    ULT_RUN = lambda task, variant, witness_dir, witness_name, input: Ultimate.ULT_CMD(
+                    ult_exe=Ultimate.ULT_EXE(variant=variant), 
+                    ult_opts=Ultimate.ULT_COMMON_OPTS,
+                    ult_task_opts=(Ultimate.ULT_REACH_OPTS(witness_dir=witness_dir, witness_name=witness_name) if task is Ultimate.ULT_REACH_TASK
+                                    else Ultimate.ULT_VALIDATE_OPTS(witness_dir=witness_dir, witness_name=witness_name)), 
+                    input=input)
--- src/validate.py
+++ src/validate.py
@@ -14,19 +14,29 @@ class Validator(object):
             mytempdir.mkdir()
         self.tmpdir = mytempdir
         self.witness_name = 'witness.graphml'
+        self.witness = self.tmpdir / self.witness_name
 
     def prove_reach(self, input):
         cwd = os.getcwd()
         try:
             os.chdir(self.tmpdir)
             assert input.is_file(), input
-            cmd = self.prove_reach_cmd(input=input)
-            mlog.debug("cmd: {}".format(cmd))
-            rmsg, errmsg = CM.vcmd(cmd)
-            assert not errmsg, "'{}': {}".format(cmd, errmsg)
+            pcmd = self.prove_reach_cmd(input=input)
+            mlog.debug("pcmd: {}".format(pcmd))
+            rmsg, errmsg = CM.vcmd(pcmd)
+            assert not errmsg, "'{}': {}".format(pcmd, errmsg)
             # mlog.debug("rmsg: {}".format(rmsg))
             res = self.parse_rmsg(rmsg)
             mlog.debug("res: {}".format(res))
+            if res is False:
+                assert self.witness.is_file(), self.witness
+                vcmd = self.validate_witness_cmd(input=input)
+                mlog.debug("vcmd: {}".format(vcmd))
+                v_rmsg, v_errmsg = CM.vcmd(vcmd)
+                # assert not v_errmsg, "'{}': {}".format(vcmd, v_errmsg)
+                mlog.debug("v_rmsg: {}".format(v_rmsg))
+                mlog.debug("v_errmsg: {}".format(v_errmsg))
+
         except Exception as ex:
             mlog.debug("Exception: {}".format(ex))
             mlog.debug(traceback.format_exc())
@@ -34,6 +44,19 @@ class Validator(object):
         finally:
             os.chdir(cwd)
             return res
+
+    def parse_rmsg(self, rmsg):
+        res_index = rmsg.find(self.res_keyword)
+        if res_index != -1:
+            res = rmsg[(res_index + len(self.res_keyword)):]
+            if 'TRUE' in res:
+                return True
+            elif 'FALSE' in res:
+                return False
+            else:
+                return None
+        else:
+            return None
 
 class CPAchecker(Validator):
     @property
@@ -50,6 +73,10 @@ class CPAchecker(Validator):
         return partial(settings.CPAchecker.CPA_CMD, 
                        cpa_task_opts=settings.CPAchecker.CPA_VALIDATE_OPTS(witness=self.witness))
 
+    @property
+    def res_keyword(self):
+        return 'Verification result:'
+
 class UAutomizer(Validator):
     @property
     def short_name(self):
@@ -57,21 +84,24 @@ class UAutomizer(Validator):
 
     @property
     def prove_reach_cmd(self):
-        return partial(settings.Ultimate.ULT_CMD, 
-                       variant="UAutomizer", 
+        return partial(settings.Ultimate.ULT_RUN, 
+                       variant="UAutomizer",
+                       task=settings.Ultimate.ULT_REACH_TASK,
                        witness_dir=self.tmpdir,
                        witness_name=self.witness_name)
 
-    def parse_rmsg(self, rmsg):
-        res_keyword = 'Result:'
-        res_index = rmsg.find(res_keyword)
-        if res_index != -1:
-            res = rmsg[(res_index + len(res_keyword)):]
-            if 'TRUE' in res:
-                return True
-            elif 'FALSE' in res:
-                return False
-            else:
-                return None
-        else:
-            return None
+    @property
+    def validate_witness_cmd(self):
+        return partial(settings.Ultimate.ULT_RUN, 
+                       variant="UAutomizer", 
+                       task=settings.Ultimate.ULT_VALIDATE_TASK,
+                       witness_dir=self.tmpdir,
+                       witness_name=self.witness_name)
+
+    @property
+    def res_keyword(self):
+        return 'Result:'
+
+    def parse_counterexample(self):
+        pass
+        
