eb73cdf0a9d3c4c635af96197b2693120d80bdd9
chanhle <letonchanh@gmail.com>
2020-05-07 16:54:50 +0000

    modified egcd

--- benchmarks/svcomp-nla-digbench/egcd-ult-t.c
+++ benchmarks/svcomp-nla-digbench/egcd-ult-t.c
@@ -1,4 +1,5 @@
 /* extended Euclid's algorithm */
+extern int __VERIFIER_nondet_int(void);
 
 int main() {
     int a, b, p, q, r, s;
@@ -7,33 +8,34 @@ int main() {
     y = __VERIFIER_nondet_int();
     //assume_abort_if_not(x >= 1);
     //assume_abort_if_not(y >= 1);
+    if (x>=1 && y>=1) {
+        a = x;
+        b = y;
+        p = 1;
+        q = 0;
+        r = 0;
+        s = 1;
 
-    a = x;
-    b = y;
-    p = 1;
-    q = 0;
-    r = 0;
-    s = 1;
+        while (y * r + x * p != x * q + y * s) {
+            //__VERIFIER_assert(1 == p * s - r * q);
+            //__VERIFIER_assert(a == y * r + x * p);
+            //__VERIFIER_assert(b == x * q + y * s);
 
-    while (y * r + x * p != x * q + y * s) {
-        //__VERIFIER_assert(1 == p * s - r * q);
-        //__VERIFIER_assert(a == y * r + x * p);
-        //__VERIFIER_assert(b == x * q + y * s);
-
-        if (a > b) {
-            a = a - b;
-            p = p - q;
-            r = r - s;
-        } else {
-            b = b - a;
-            q = q - p;
-            s = s - r;
+            if (a > b) {
+                a = a - b;
+                p = p - q;
+                r = r - s;
+            } else {
+                b = b - a;
+                q = q - p;
+                s = s - r;
+            }
         }
-    }
     
-    //__VERIFIER_assert(a - b == 0);    
-    //__VERIFIER_assert(p*x + r*y - b == 0);
-    //__VERIFIER_assert(q*r - p*s + 1 == 0);
-    //__VERIFIER_assert(q*x + s*y - b == 0);
+    	//__VERIFIER_assert(a - b == 0);    
+    	//__VERIFIER_assert(p*x + r*y - b == 0);
+    	//__VERIFIER_assert(q*r - p*s + 1 == 0);
+    	//__VERIFIER_assert(q*x + s*y - b == 0);
+    }
     return 0;
 }
