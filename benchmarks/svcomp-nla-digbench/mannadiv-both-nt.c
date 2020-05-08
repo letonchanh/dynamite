499753ca9f71bf2bae81c4b5a6046dfb860155ee
Timos Antonopoulos <timos@ip-172-31-63-194.us-west-2.compute.internal>
2020-05-08 20:47:31 +0000

    mannadiv benchmark

---
+++ benchmarks/svcomp-nla-digbench/mannadiv-both-nt.c
@@ -0,0 +1,42 @@
+/* 
+ Division algorithm from
+ "Z. Manna, Mathematical Theory of Computation, McGraw-Hill, 1974"
+ return x1 // x2
+*/
+
+extern int __VERIFIER_nondet_int(void);
+
+int main() {
+    int x1, x2;
+    int y1, y2, y3;
+    x1 = __VERIFIER_nondet_int();
+    x2 = __VERIFIER_nondet_int();
+
+    // assume_abort_if_not(x1 >= 0);
+    // assume_abort_if_not(x2 != 0);
+
+    if (x1 < 0 || x2 == 0) {
+        return 0;
+    }
+
+    y1 = 0;
+    y2 = 0;
+    y3 = x1;
+
+    while (1) {
+        // __VERIFIER_assert(y1*x2 + y2 + y3 == x1);
+
+        if (!(y1*x2 + y2 + y3 == x1)) break;
+
+        if (y2 + 1 == x2) {
+            y1 = y1 + 1;
+            y2 = 0;
+            y3 = y3 - 1;
+        } else {
+            y2 = y2 + 1;
+            y3 = y3 - 1;
+        }
+    }
+    // __VERIFIER_assert(y1*x2 + y2 == x1);
+    return 0;
+}
