bd70334f7c02e052da0f344b3d05945032b4754f
Eric Koskinen <eric.koskinen@stevens.edu>
2020-05-08 03:17:22 +0000

    freire1.c

---
+++ benchmarks/svcomp-nla-digbench/freire1.c
@@ -0,0 +1,38 @@
+/* Algorithm for finding the closest integer to square root,
+ * more details, see : http://www.pedrofreire.com/sqrt/sqrt1.en.html 
+
+Note: for some reason using cpa was able to disprove these
+cpa.sh -kInduction -setprop solver.solver=z3 freire1.c
+*/
+
+extern void __VERIFIER_error() __attribute__((__noreturn__));
+extern double __VERIFIER_nondet_double(void);
+extern void __VERIFIER_assume(int expression);
+void __VERIFIER_assert(int cond) {
+    if (!(cond)) {
+    ERROR:
+        __VERIFIER_error();
+    }
+    return;
+}
+
+int main() {
+    int r;
+    //double a, x;
+    int a, x;
+    a = __VERIFIER_nondet_int();
+    x = a / 2.0;
+    r = 0;
+    int c = 0; int k; // Added
+    
+    while ( r*r - a - r + 2*x + c <= k ) {
+      //__VERIFIER_assert((int)(r*r - a - r + 2*x) == 0);
+
+      //if (!(x > r))             break;
+        x = x - r;
+        r = r + 1;
+    }
+
+    //__VERIFIER_assert((int)(r*r - a - r + 2*x) == 0);
+    return 0;
+}
