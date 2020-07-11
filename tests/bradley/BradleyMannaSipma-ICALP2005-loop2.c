1b385de8919685949f527976f956a919638e2db6
chanhle <letonchanh@gmail.com>
2020-07-11 11:10:57 -0500

    add Bradley's examples

---
+++ tests/bradley/BradleyMannaSipma-ICALP2005-loop2.c
@@ -0,0 +1,34 @@
+/*
+ * Program from Fig.1 of
+ * 2005ICALP - Bradley,Manna,Sipma - The Polyranking Principle
+ *
+ * Date: 12.12.2013
+ * Author: heizmann@informatik.uni-freiburg.de
+ *
+ */
+
+typedef enum {false, true} bool;
+
+extern int __VERIFIER_nondet_int(void);
+
+int __VERIFIER_nondet_pos() {
+	int n = __VERIFIER_nondet_int();
+	if (n > 0)
+		return n;
+	else
+		return -n + 1;
+}
+
+int main() {
+    	int x, y, z;
+	x = __VERIFIER_nondet_int();
+	y = __VERIFIER_nondet_int();
+	while (x >= y) {
+		if (__VERIFIER_nondet_int()) {
+			x = x - 1 - __VERIFIER_nondet_pos();
+    		} else {
+	    		y = y + 1 + __VERIFIER_nondet_pos();
+		}
+	}
+	return 0;
+}
