79a83e2255db2565b3883b919c50174b8b974b23
chanhle <letonchanh@gmail.com>
2020-08-06 21:33:13 -0500

    improved loop transrel

--- benchmarks/nla-nonterm/mannadiv-both-nt.c
+++ benchmarks/nla-nonterm/mannadiv-both-nt.c
@@ -28,12 +28,12 @@ int main() {
 
         if (!(y1*x2 + y2 + y3 == x1)) break;
 
-        if (y2 + 1 != x2) {
-            y2 = y2 + 1;
-            y3 = y3 - 1;
-        } else {
+        if (y2 + 1 == x2) {
             y1 = y1 + 1;
             y2 = 0;
+            y3 = y3 - 1;
+        } else {
+            y2 = y2 + 1;
             y3 = y3 - 1;
         }
     }
