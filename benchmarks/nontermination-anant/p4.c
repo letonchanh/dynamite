6fdc883ddf81a9499a2075e7c3d5042ca33c5ce1
chanhle <letonchanh@gmail.com>
2020-09-10 21:50:34 -0400

    update p4

--- benchmarks/nontermination-anant/p4.c
+++ benchmarks/nontermination-anant/p4.c
@@ -3,45 +3,6 @@ typedef enum {false, true} bool;
 
 extern int __VERIFIER_nondet_int(void);
 
-void f(int w, int x, int y, int z) {
-  if (! (z >= 4)) { return; }
-  z = z + 1;
-
-  if (x >= 0) {
-    if (! (w <= -5)) { return; }
-    z = z + 1;
-  } else {
-    z = z - 1;
-  }
-
-  if (__VERIFIER_nondet_int() > 0) {
-    if (! (x < 0)) { return; }
-    z = z - 1;
-    return;
-  }
-  while (x >= w) {
-    if (__VERIFIER_nondet_int() > 0) {
-      if (! (x < 0)) { return; }
-      z = z - 1;
-      return;
-    }
-    /*
-    if (z <= 8) {
-      ;
-    } else {
-      ;
-    }
-    */
-    x = z * z;
-    w = w - 1;
-  }
-  if (__VERIFIER_nondet_int() > 0) {
-    if (! (x < 0)) { return; }
-    z = z - 1;
-    return;
-  }
-}
-
 int main() {
   int w, x, y, z;
   w = __VERIFIER_nondet_int();
@@ -49,6 +10,52 @@ int main() {
   y = __VERIFIER_nondet_int();
   z = __VERIFIER_nondet_int();
 
-  f(w, x, y, z);
+  if (z >= 4) {
+    z = z + 1;
+
+    if (x >= 0) {
+      if (w <= -5) {
+	z = z + 1;
+      }
+    } else {
+      z = z - 1;
+    }
+
+    if (x < 0 || w <= -5) {
+      /*
+      if (__VERIFIER_nondet_int() > 0) {
+	if (! (x < 0)) { return; }
+	z = z - 1;
+	return;
+      }
+      */
+      while (x >= w) {
+	/*
+	if (__VERIFIER_nondet_int() > 0) {
+	  if (! (x < 0)) { return; }
+	  z = z - 1;
+	  return;
+	}
+	*/
+	/*
+	  if (z <= 8) {
+	  ;
+	  } else {
+	  ;
+	  }
+	*/
+	x = z * z;
+	w = w - 1;
+      }
+      /*
+      if (__VERIFIER_nondet_int() > 0) {
+	if (! (x < 0)) { return; }
+	z = z - 1;
+	return;
+      }
+      */
+    }
+  }
+  
   return 0;
 }
